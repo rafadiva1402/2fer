58856d8207f85f25a1cc7657188613f30ec3cf8d
rafadiva1402 <161479749+rafadiva1402@users.noreply.github.com>
2024-03-25 11:08:54 -0300

    Add files via upload

---
+++ 2fer (1).py
@@ -0,0 +1,14 @@
+print("2-fer")
+
+def Grande():
+    nome = input("me informe seu nome")
+    
+    if not nome:
+        nome = ("voce")
+    else:
+        pass
+    
+    print(f"um biscoito para {nome}, um biscoito para mim")
+    
+Grande()
+
